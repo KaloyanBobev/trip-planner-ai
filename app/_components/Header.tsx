89410c13cbaa2254c698550d2d2c2cc2e26d39cf
Kaloyan <kaloyanbobev@gmail.com>
2025-10-14 12:15:56 +0100

    create a Header component

---
+++ app/_components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image';
+function Header() {
+  return (
+    <div>
+      {/*Logo*/}
+      <div className='flex gap-2 item-center'>
+        <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
+        <h2 className='font-bold text-2xl'>AI Trip planner</h2>
+      </div>
+      {/*Menu options*/}
+      {/*Get started button*/}
+    </div>
+  );
+}
+
+export default Header
