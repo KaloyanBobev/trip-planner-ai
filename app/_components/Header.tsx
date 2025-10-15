256ffa19a5b99fcd9df4c3ce66bac7ec44ac7089
Kaloyan <kaloyanbobev@gmail.com>
2025-10-15 12:26:06 +0100

    update the file with menu option

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+
+const menuOptions = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Pricing",
+    path: "/pricing",
+  },
+];
+
 function Header() {
   return (
     <div>
       {/*Logo*/}
-      <div className='flex gap-2 item-center'>
+      <div className="flex gap-2 item-center">
         <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
-        <h2 className='font-bold text-2xl'>AI Trip planner</h2>
+        <h2 className="font-bold text-2xl">AI Trip planner</h2>
       </div>
       {/*Menu options*/}
       {/*Get started button*/}
@@ -14,4 +26,4 @@ function Header() {
   );
 }
 
-export default Header
+export default Header;
