290fce3b568df3c214401dd97762051d40516fc6
Kaloyan <kaloyanbobev@gmail.com>
2025-10-18 23:49:06 +0100

    write a new hero section in the website with some structure

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="mt-24 w-full flex justify-center">
+      {/*Content */}
+      <div className="max-w-3xl w-full text-center">
+        <h1 className="text-xl md:text-5xl font-bold">
+          Hey, I'm your personal{" "}
+          <span className="text-primary">Trip planner</span>
+        </h1>
+        <p></p>
+        {/*Import Box  */}
+
+        {/*Sugesstion list */}
+        {/*Video Selection  */}
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
