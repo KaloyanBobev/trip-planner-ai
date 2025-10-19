078cf37b72e60cd727c9c0272841c4b800e3910c
Kaloyan <kaloyanbobev@gmail.com>
2025-10-19 15:00:25 +0100

    crate a Hero section including header, textbox and video

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,19 +1,76 @@
 import React from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Globe2, Landmark, Plane, Send } from "lucide-react";
+import { HeroVideoDialog } from "@/components/ui/hero-video-dialog";
+
+const suggestions = [
+  {
+    title: "Create a new trip",
+    icon: <Globe2 className="text-blue-400 h-5 w-5" />,
+  },
+  {
+    title: "Inspire me with a destination",
+    icon: <Plane className="text-green-500 h-5 w-5" />,
+  },
+  {
+    title: "Discover popular places",
+    icon: <Landmark className="text-orange-500 h-5 w-5" />,
+  },
+  {
+    title: "Adveture destinations",
+    icon: <Globe2 className="text-yellow-600 h-5 w-5" />,
+  },
+];
 
 function Hero() {
   return (
     <div className="mt-24 w-full flex justify-center">
       {/*Content */}
-      <div className="max-w-3xl w-full text-center">
+      <div className="max-w-3xl w-full text-center space-y-6">
         <h1 className="text-xl md:text-5xl font-bold">
           Hey, I'm your personal{" "}
           <span className="text-primary">Trip planner</span>
         </h1>
-        <p></p>
+        <p className="text-lg">
+          Tell me what you want, and I'll handle the rest: Flights, Hotels, trip
+          Planner - all in second
+        </p>
         {/*Import Box  */}
-
+        <div>
+          {/* make this container `relative` so the absolutely positioned button
+              is placed relative to the input box instead of the page */}
+          <div className="relative border rounded-2xl p-4 ">
+            <Textarea
+              placeholder="Create a trip from Paris to New York"
+              className="w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none"
+            />
+            <Button size={"icon"} className="absolute bottom-6 right-6">
+              <Send className="h-4 w-4" />
+            </Button>
+          </div>
+        </div>
         {/*Sugesstion list */}
+        <div className="flex gap-5">
+          {suggestions.map((suggestions, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-2 border rounded-full p-2 cursor-pointer hover:bg-primary hover:text-white"
+            >
+              {suggestions.icon}
+              <h2 className="text-xs">{suggestions.title}</h2>
+            </div>
+          ))}
+        </div>
         {/*Video Selection  */}
+
+        <HeroVideoDialog
+          className="block dark:hidden"
+          animationStyle="from-center"
+          videoSrc="https://www.example.com/dummy-video"
+          thumbnailSrc="https://mma.prnewswire.com/media/2401528/1_MindtripProduct.jpg?p=facebook"
+          thumbnailAlt="Dummy Video Thumbnail"
+        />
       </div>
     </div>
   );
