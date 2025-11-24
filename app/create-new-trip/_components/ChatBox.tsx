bea60d8b6fbd4f1cc9a4e724a861632b44843177
Kaloyan <kaloyanbobev@gmail.com>
2025-11-24 21:58:03 +0000

    Update ChatBox.tsx

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -42,18 +42,21 @@ function ChatBox() {
     <div className="h-[85vh] flex flex-col">
       {/*Display Messsages*/}
       <section className="flex-1 overflow-y-auto p-4">
-        <div className="flex justify-end mt-2">
-          <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
-            {" "}
-            User Msg
-          </div>
-        </div>
-        <div className="flex justify-start mt-2">
-          <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
-            {" "}
-            AI Agent Msg
-          </div>
-        </div>
+        {messages.map((msg: Message, index) =>
+          msg.role == "user" ? (
+            <div className="flex justify-end mt-2" key={index}>
+              <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
+                {msg.content}
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-start mt-2" key={index}>
+              <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
+                {msg.content}
+              </div>
+            </div>
+          )
+        )}
       </section>
       {/* User Input  */}
       <section>
