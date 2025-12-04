62d3467f52b901f3e1248eb9517895b42cf082f0
Kaloyan <kaloyanbobev@gmail.com>
2025-12-04 20:50:56 +0000

    Update GroupSizeUi.tsx

--- app/create-new-trip/_components/GroupSizeUi.tsx
+++ app/create-new-trip/_components/GroupSizeUi.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+export const SelectTravelesList = [
+  {
+    id: 1,
+    title: "Just Me",
+    desc: "A sole traveles in exploration",
+    icon: "🧍‍♂️",
+    people: "1",
+  },
+  {
+    id: 2,
+    title: "A Couple",
+    desc: "Two traveles in tandem",
+    icon: "🧍‍♂️🧍‍♀️",
+    people: "2 People",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "A group of fun loving adv",
+    icon: "🏕️",
+    people: "3 to 5 People",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    desc: "A bunch of thrill-seekes",
+    icon: "⛺",
+    people: "5 to 10 People",
+  },
+];
+
 function GroupSizeUi() {
-  return (
-    <div>GroupSizeUi</div>
-  )
+  return <div></div>;
 }
 
 export default GroupSizeUi
