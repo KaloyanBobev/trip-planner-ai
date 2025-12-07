0f7101aacb1d9297312ef47c0578c759271369a9
Kaloyan <kaloyanbobev@gmail.com>
2025-12-07 19:08:28 +0000

    change some routes letters

--- app/api/aimodel/route.tsx
+++ app/api/aimodel/route.tsx
@@ -19,10 +19,10 @@ Only ask questions about the following details in order, and wait for the user's
 Do not ask multiple questions at once, and never ask irrelevant questions.
 If any answer is missing or unclear, politely ask the user to clarify before proceeding.
 Always maintain a conversational, interactive style while asking questions.
-Along with response also send which ui component to display for generative UI for example 'budget/groupSize/TripDuration/final), where Final means Al generating complete final outpur Once all required information is collected, generate and return a strict JSON response only (no explanations or extra text) with following JSON schema:
+Along with response also send which ui component to display for generative UI for example 'budget/groupSize/tripDuration/final), where Final means Al generating complete final outpur Once all required information is collected, generate and return a strict JSON response only (no explanations or extra text) with following JSON schema:
 {
 resp:'Text Resp',
-ui:"budget/groupSize/TripDuration/final)'
+ui:"budget/groupSize/tripDuration/final)'
 }
 `;
 
